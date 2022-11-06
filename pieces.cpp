6d55ac848eb0d745c64e77205a6da308beee3c58
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-06 23:05:17 +0200

    032

--- pieces.cpp
+++ pieces.cpp
@@ -1,6 +1,10 @@
 #include "pieces.hpp"
 #include "gameboard.hpp"
 
+auto isOpponent = [](const GameBoard& board, const stf::Vec2d &p0, const stf::Vec2d &p1) -> bool {
+    return board[{p1}]->getOpponent() == board[{p0}]->color();
+};
+
 uint8_t Pawn::view() const
 {
     return 'P';
@@ -9,8 +13,26 @@ uint8_t Pawn::view() const
 bool Pawn::canAttack(const GameBoard& board, const stf::Vec2d &selected, const stf::Vec2d &selectable) const
 {
     const stf::Vec2d pos = selectable - selected;
-    if(std::abs(pos.x) == 1 && std::abs(pos.y) == 1 && board[{selectable}]->getOpponent() == board[{selected}]->color()) {
+    if(std::abs(pos.x) == 1 && std::abs(pos.y) == 1 && isOpponent(board, selected, selectable)) {
         return true;
-    }
-    return false;
+    } return false;
+}
+
+uint8_t Queen::view() const
+{
+    return 'Q';
+}
+
+bool Queen::canAttack(const GameBoard &board, const stf::Vec2d &selected, const stf::Vec2d &selectable) const
+{
+    const stf::Vec2d pos = selectable - selected;
+    const stf::Vec2d posAbs = { std::abs(pos.x), std::abs(pos.y) };
+
+    const bool diagonals = posAbs.x == posAbs.y;
+    const bool vertical  = selected.x != selectable.x && selected.y == selectable.y;
+    const bool horizontal= selected.x == selectable.x && selected.y != selectable.y;
+
+    if ((diagonals || vertical || horizontal) && isOpponent(board, selected, selectable)) {
+        return true;
+    } return false;
 }
--- pieces.hpp
+++ pieces.hpp
@@ -10,4 +10,11 @@ public:
     bool canAttack(const GameBoard &board, const stf::Vec2d& selected, const stf::Vec2d& selectable) const final;
 };
 
+class Queen : virtual public BoardCell
+{
+public:
+    uint8_t view() const override;
+    bool canAttack(const GameBoard &board, const stf::Vec2d& selected, const stf::Vec2d& selectable) const final;
+};
+
 #endif // PIECES_HPP
